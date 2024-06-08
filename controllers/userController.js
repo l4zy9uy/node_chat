e388f208eddc95700ae97b4c93d365e770dcd6ef
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-08 16:44:40 +0700

    feat: add codes onGetAllUsers and onGetUserById

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,33 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 
 exports.onGetAllUsers = asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(500).json({ success: false, errors: errors.array() });
+    }
 
+    const users = await User.find();
+    return res.status(200).json({success: true, users});
 });
-exports.onGetUserById = asyncHandler(async (req, res, next) => { });
+exports.onGetUserById = asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            // Instead of throwing an error, send a 404 response
+            return res.status(404).json({ success: false, error: "No user with this id found" });
+        }
+        console.log(user);
+        return res.status(200).json({ success: true, user });
+    } catch (err) {
+        // Pass errors to the error handler
+        next(err);
+    }
+});
 exports.onCreateUser = [
     body("username")
         .isLength({min: 6, max: 32})
