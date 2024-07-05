9996b301b94e6566475eea62c8fa06d037e4c306
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-07-05 17:44:52 +0700

    refactor: Refactor server.js code

--- server.js
+++ server.js
@@ -3,32 +3,37 @@ const app = express();
 const mongoose = require('mongoose');
 const http = require('http');
 const bodyParser = require('body-parser');
-const {Server: Server} = require('socket.io');
-const httpServer = http.createServer(app);
-const io = new Server(httpServer);
+const Server = require('socket.io').Server;
 const logger = require("morgan");
-require('dotenv').config();
-const mongoDb = process.env.DB_URI;
-const port = process.env.PORT || "8080";
 const session = require('express-session');
 const flash = require('connect-flash');
+const dotenv = require('dotenv');
+dotenv.config();
 
+// Constants for the server
+const mongoDb = process.env.DB_URI;
+const port = process.env.PORT || "8080";
+
+//Routers
 const indexRouter = require('./routes/index');
 const messageRouter = require('./routes/message');
 const authRouter = require('./routes/auth');
 
+// Server and Socket.io setup
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+// View and Static File Setup
 app.set('view-engine', 'pug');
 app.set("port", port);
-
-app.use(logger("dev"));
 app.use(express.static(__dirname));
+
+// Login and Body Parsing
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use((req, res, next) => {
-    req.io = io;
-    next();
-});
 
+// Session Configuration
 const sessionMiddleware = session({
     secret: 'your secret',
     resave: true,
@@ -37,14 +42,18 @@ const sessionMiddleware = session({
 
 app.use(sessionMiddleware);
 io.engine.use(sessionMiddleware);
+
+// Flash Messages
 app.use(flash());
 app.use((req, res, next) => {
+    req.io = io;
     res.locals.error = req.flash('error');
     res.locals.success_message = req.flash('success_message');
     res.locals.error_registration_message = req.flash('error_registration_message');
     next();
 });
 
+// Routes
 app.use("/", indexRouter);
 app.use("/", authRouter);
 app.use("/messages", messageRouter);
@@ -55,6 +64,7 @@ async function main() {
     await mongoose.connect(mongoDb);
 }
 
+// Socket.io Events
 io.on('connection', (socket) => {
     console.log('a user connected');
     try {
@@ -66,6 +76,7 @@ io.on('connection', (socket) => {
     }
 });
 
+// Server Initialization
 httpServer.listen(port, () => {
     console.log(`Express server listening on ${port}`);
 });
